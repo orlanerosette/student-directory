33353bc83ac7bc09e66e37bf6022aaa81e507ebf
Orlane <orlanedoumbe@gmail.com>
2022-03-02 13:04:43 +0000

    Added the student count and comments explaining what we did

--- directory.rb
+++ directory.rb
@@ -1,3 +1,4 @@
+# first we printed list of students using 'puts'
 puts "The students of Villains Academy"
 puts "----------------"
 puts "Dr. Hannibal Lecter"
@@ -11,3 +12,8 @@ puts "Freddy Krueger"
 puts "The Joker"
 puts "Joffrey Baratheon"
 puts "Norman Bates"
+
+# then we printed the total number of students using 'print'
+print "Overall we have "
+print 11
+print " great students"
