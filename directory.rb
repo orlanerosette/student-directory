34f8591f749d09121de0f566732abeeb5f282c3e
Orlane <orlanedoumbe@gmail.com>
2022-03-02 13:14:25 +0000

    More refactoring, created a students array and used a block to 'puts' each student

--- directory.rb
+++ directory.rb
@@ -1,20 +1,24 @@
-# created student count variable
-student_count = 11
+
+# created student array
+students = [
+  "Dr. Hannibal Lecter",
+  "Darth Vader",
+  "Nurse Ratched",
+  "Michael Corleone",
+  "Alex DeLarge",
+  "The Wicked Witch of the West",
+  "Terminator",
+  "Freddy Krueger",
+  "The Joker",
+  "Joffrey Baratheon",
+  "Norman Bates"
+]
 
 # printed list of students using 'puts'
 puts "The students of Villains Academy"
 puts "----------------"
-puts "Dr. Hannibal Lecter"
-puts "Darth Vader"
-puts "Nurse Ratched"
-puts "Michael Corleone"
-puts "Alex DeLarge"
-puts "The Wicked Witch of the West"
-puts "Terminator"
-puts "Freddy Krueger"
-puts "The Joker"
-puts "Joffrey Baratheon"
-puts "Norman Bates"
+students.each { |student| puts student }
+
 
 # then we printed the total number of students using 'print'
-puts "Overall we have #{student_count} great students."
+puts "Overall we have #{students.count} great students."
